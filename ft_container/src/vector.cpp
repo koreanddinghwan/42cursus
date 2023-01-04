623fc3f64570bf63e2df1e18892773797518aa10
koreanddinghwan <myunghwan0421@gmail.com>
2023-01-04 19:50:02 +0900

    FEAT:vector hpp main

--- ft_container/src/vector.cpp
+++ ft_container/src/vector.cpp
@@ -1,18 +1,15 @@
 #include "vector.hpp"
+#include <vector>
 
-int main(void){
-    {
-    ft::vector<int> first;
-    }
-    std::cout<<std::endl;
-    std::cout<<std::endl;
-    std::cout<<std::endl;
-    std::cout<<std::endl;
-    {
-	ft::vector<int> second(4, 100);
-    }
-    {
-	/* ft::vector<int> third(second.begin(), second.end()); */
-    }
-    return 0;
+int main(void) {
+  { ft::vector<int> first; }
+  std::cout << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
+  {
+    ft::vector<int> second(4, 100);
+    /* ft::vector<int> third(second.begin(), second.end()); */ 
+  }
+  return 0;
 }
