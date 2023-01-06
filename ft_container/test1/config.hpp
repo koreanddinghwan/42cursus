066b19a3e10e330ad904af514aabb4fc5eaff396
myukang <myunghwan0421@gmail.com>
2023-01-06 14:56:38 +0900

    REMOVE:test1

---
+++ ft_container/test1/config.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+/**************************************************/
+/*			Change paths to your files			  */
+/*				or comment #include */
+/*		if you haven't yet some container		  */
+/**************************************************/
+#define VECTOR "../../../src/vector/vector.hpp"
+#define STACK "../../../src/stack/stack.hpp" * /
+#define MAP "../../../src/map/map.hpp" * /
+#define SET "../../../src/set/set.hpp" * /
+#define UTILITIES "../../../src/utils/utils.hpp"
+
+/* comment this to turn OFF flags (-WWW, -std=98) */
+/* #define FLAGS */
+
+/**************************************************/
+/*		Change template names of your classes	  */
+/**************************************************/
+#define _vector ft::vector
+#define _stack ft::stack
+#define _map ft::map
+#define _set ft::set
+#define _is_integral ft::is_integral
+#define _enable_if ft::enable_if
+#define _pair ft::pair
+#define _make_pair ft::make_pair
+#define _lexicographical ft::lexicographical_compare
+#define _equal ft::equal
