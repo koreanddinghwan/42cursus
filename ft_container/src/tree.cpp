429d1aed6a00ca6b6ea4d334754101870afca139
myukang <myunghwan0421@gmail.com>
2023-01-24 22:37:11 +0900

    FEAT: rebalancing

--- ft_container/src/tree.cpp
+++ ft_container/src/tree.cpp
@@ -65,20 +65,19 @@ const _rb_tree_centinel_node* _rb_tree_const_decrement(const _rb_tree_centinel_n
 	return _tree_decrement(const_cast<_rb_tree_centinel_node *>(__n));	
 }
 
-_rb_tree_centinel_node *_get_node_grand_parent(_rb_tree_centinel_node *__n) {
 
-	if (__n->_parent->_parent == __n)
-		return NULL;
-	else
-		return (__n->_parent->_parent);
+
+/*
+ * utils
+ * */
+
+_rb_tree_centinel_node *_get_node_grand_parent(_rb_tree_centinel_node *__n) {
+	return (__n->_parent->_parent);
 }
 
 _rb_tree_centinel_node *_get_node_uncle(_rb_tree_centinel_node *__n) {
 	//부모의 오른쪽 자식인 경우,
 	_rb_tree_centinel_node *_gp = _get_node_grand_parent(__n);
-
-	if (!_gp)
-		return NULL;
 
 	if (_gp->_right == __n->_parent)
 		return (_gp->_left);
@@ -94,23 +93,140 @@ const bool _is_root(_rb_tree_centinel_node *__n) {
 		return false;
 }
 
+const bool _is_leaf(_rb_tree_centinel_node *__n) {
+	if (__n->_color == _black
+		&& !(__n->_left) && !(__n->_right)
+		)
+		return true;
+	else
+		return false;
+}
+
+
+/*
+ * rotate method
+ * */
+
+/*
+ *          __p                         __p
+ *
+ *               __n       =>                   __c
+ *
+ *
+ *          __b        __c                 __n       __cr
+ *                     |  |                |  |
+ *                 __cl   __cr           __b  __cl
+ * */
+void _rb_tree_rotate_left(
+		_rb_tree_centinel_node * const __n,
+		_rb_tree_centinel_node* &__root) {
+	
+	_rb_tree_centinel_node *__c = __n->_right;
+
+	//__cl
+	__n->_right = __c->_left;
+	if (__c->_left != NULL)
+		__c->_left->_parent = __n;
+	__c->_parent = __n->_parent;
+
+	//__p
+	if (__n == __root)//header node specification
+		__root = __c;
+	//__n is left child of __p -> now, __c is left child of __p
+	else if (__n == __n->_parent->_left)
+		__n->_parent->_left = __c;
+	else
+		__n->_parent->_right = __c;
+
+	__c->_left = __n;
+	__n->_parent = __c;
+}
+
+/*
+ *                  __p                     __p
+ *
+ *
+ *            __n         =>           __c
+ *
+ *
+ *      __c      __b               __cl      __n
+ *      | |                                  |  |
+ *   __cl __cr                            __cr  __b
+ * */
+void _rb_tree_rotate_right(
+		_rb_tree_centinel_node * const __n,
+		_rb_tree_centinel_node* &__root) {
+	
+	_rb_tree_centinel_node *__c = __n->_left;
+
+	//__cr
+	__n->_left = __c->_right;
+	if (__c->_right != NULL)
+		__c->_right->_parent = __n;
+	__c->_parent = __n->_parent;
+
+	//__p
+	if (__n == __root)
+		__root = __c;
+	else if (__n == __n->_parent->_left)
+		__n->_parent->_left = __c;
+	else
+		__n->_parent->_right = __c;
+
+	__c->_right = __n;
+	__n->_parent = __c;
+}
+
+
 void rebalancing_case1(
 		_rb_tree_centinel_node *__n,
-		_rb_tree_centinel_node *__p,
 		_rb_tree_centinel_node &__h
 );
 
+//ll, rr case
+void rebalancing_case5(
+		_rb_tree_centinel_node *__n,
+		_rb_tree_centinel_node &__h
+		) {
+	_rb_tree_centinel_node *__gp = _get_node_grand_parent(__n);
+
+	__n->_parent->_color = _black;
+	__gp->_color = _red;
+	if (__n == __n->_parent->_left) //ll case
+		_rb_tree_rotate_right(__gp, __h._parent);
+	else //rr case
+		_rb_tree_rotate_left(__gp, __h._parent);
+}
+
+// lr, rl case to ll, rr case
 void rebalancing_case4(
 		_rb_tree_centinel_node *__n,
-		_rb_tree_centinel_node *__p,
 		_rb_tree_centinel_node &__h
 		) {
 
+	_rb_tree_centinel_node *__gp = _get_node_grand_parent(__n);
+
+	if (//lr case to ll case
+		__n == __n->_parent->_right
+		&& __n->_parent == __gp->_left
+		) 
+	{
+		_rb_tree_rotate_left(__n->_parent, __h._parent);
+		__n = __n->_left; //old parent
+	}
+	else if (//rl case to rr case
+			__n == __n->_parent->_left
+			&& __n->_parent == __gp->_right
+			) 
+	{
+		_rb_tree_rotate_right(__n->_parent, __h._parent);
+		__n = __n->_right;
+	}//ll, rr case and transformed ll, rr case by upper if, else if
+	rebalancing_case5(__n, __h);
 }
 
 void rebalancing_case3 (
 		_rb_tree_centinel_node *__n,
-		_rb_tree_centinel_node *__p,
 		_rb_tree_centinel_node &__h
 		) {
 	_rb_tree_centinel_node *__gp;
@@ -121,34 +237,31 @@ void rebalancing_case3 (
 		__u->_color = _black;
 		__gp = _get_node_grand_parent(__n);
 		__gp->_color = _red;
-		rebalancing_case1(__gp, __gp->_parent, __h);
+		rebalancing_case1(__gp, __h);
 	} else {
-
+		rebalancing_case4(__n, __h);
 	}
-
 }
 
 void rebalancing_case2 (
 		_rb_tree_centinel_node *__n,
-		_rb_tree_centinel_node *__p,
 		_rb_tree_centinel_node &__h
 		) {
-	if (__p->_color == _black)
+	if (__n->_parent->_color == _black)
 		return ;
 	else
-		rebalancing_case3(__n, __p, __h);
+		rebalancing_case3(__n, __h);
 }
 
 void rebalancing_case1 (
 		_rb_tree_centinel_node *__n,
-		_rb_tree_centinel_node *__p,
 		_rb_tree_centinel_node &__h
 		) {
 	
 	if (_is_root(__n))
 		__n->_color = _black;
 	else
-		rebalancing_case2(__n, __p, __h);
+		rebalancing_case2(__n, __h);
 }
 
 
